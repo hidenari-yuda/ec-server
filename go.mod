6d69e307d7d2426eecb64087fe3ee2d4dba2c575
hidenari-yuda <dehinari7302@icloud.com>
2022-07-21 17:26:55 +0900

    20220721 plus edit

---
+++ go.mod
@@ -0,0 +1,26 @@
+module github.com/hidenari-yuda/todo_app
+
+go 1.18
+
+require (
+	github.com/google/uuid v1.3.0
+	gopkg.in/go-ini/ini.v1 v1.66.6
+)
+
+require (
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
+
+require (
+	github.com/mattn/go-sqlite3 v1.14.14
+	github.com/stretchr/testify v1.8.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"github.com/hidenari-yuda/todo_app/app/controllers"
+)
+
+func main() {
+
+	//fmt.Println(models.Db)
+
+	/*u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.PassWord = "test1234"
+	fmt.Println(u)
+
+	u.CreateUser()*/
+
+	/*user, _ := models.GetUserByEmail("test@example.com")
+	fmt.Println(user)
+
+	session, _ := user.CreateSession()
+
+	fmt.Println(session)
+
+	valid, _ := session.CheckSession()
+	fmt.Println(valid)*/
+
+	/*fmt.Println(config.Config.SQLDriver)
+	fmt.Println(config.Config.DbName)*/
+	/*user, _ := models.GetUser(2)
+
+	user.CreateTodo("First Todo")*/
+
+	/*user, _ := models.GetUser(3)
+
+	user.CreateTodo("Third Todo")
+
+	/*todos, _ := models.GetTodos()
+	for _, v := range todos {
+		fmt.Println(v)
+	}*/
+
+	/*user2, _ := models.GetUser(2)
+	todos, _ := user2.()
+	for _, v := range todos {
+		fmt.Println(v)
+	}*/
+
+	/*t, _ := models.GetTodo(3)
+	t.DeleteTodo()*/
+
+	controllers.StartMainServer()
+
+}
